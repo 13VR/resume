d589530c4438febfdabc2dea3a96260e32ddeedd
Vitaliy Radchenko <rad4enko.vitaliy@gmail.com>
2023-05-23 22:05:53 +0300

    newLesson

--- src/route/index.js
+++ src/route/index.js
@@ -1260,4 +1260,15 @@ router.get('/bootstrapNew', function (req, res) {
   })
 })
 
+// ================================================================
+
+router.get('/navigation', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('navigation', {
+    layout: 'basic',
+  })
+})
+
 module.exports = router
