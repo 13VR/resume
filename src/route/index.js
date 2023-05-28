4a99f3ff85a1ba2b31ae349c9d4376ed5e868c73
Vitaliy Radchenko <rad4enko.vitaliy@gmail.com>
2023-05-28 19:04:36 +0300

    index8

--- src/route/index.js
+++ src/route/index.js
@@ -2462,4 +2462,13 @@ router.get('/shopcart', function (req, res) {
 
 // ================================================================
 
+router.get('/index8', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index8', {
+    layout: 'index',
+  })
+})
+
 module.exports = router
